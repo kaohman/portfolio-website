90644b741c68823c3d0af9675a8948bc154d1a00
Karin Ohman <kaohma@gmail.com>
2019-01-14 16:55:06 -0700

    Add grid styling

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,18 +4,20 @@ function Portfolio() {
   return (
     <div>
       <h1>portfolio</h1>
-      <a href='https://github.com/kaohman/national-parks'>
-        <h3>mark my parks</h3>
-        <img className='screenshots' src='./assets/parks-screenshot.png' alt='mark my parks website'></img>
-      </a>
-      <a href='https://github.com/kaohman/jeopardy'>
-        <h3>space jeopardy</h3>
-        <img className='screenshots' src='./assets/jeopardy-screenshot.png' alt='sapce jeopardy website'></img>
-      </a>
-      <a href='https://github.com/kaohman/js-proto-fun-time'>
-        <h3>jsProtoFunTime</h3>
-        <img className='screenshots' src='./assets/prototypes-screenshot.png' alt='javascript prototypes website'></img>
-      </a>
+      <div id='portfolio-content'>
+        <a href='https://github.com/kaohman/national-parks'>
+          <h3>mark my parks</h3>
+          <img className='screenshots' src='./assets/parks-screenshot.png' alt='mark my parks website'></img>
+        </a>
+        <a href='https://github.com/kaohman/jeopardy'>
+          <h3>space jeopardy</h3>
+          <img className='screenshots' src='./assets/jeopardy-screenshot.png' alt='sapce jeopardy website'></img>
+        </a>
+        <a href='https://github.com/kaohman/js-proto-fun-time'>
+          <h3>jsProtoFunTime</h3>
+          <img className='screenshots' src='./assets/prototypes-screenshot.png' alt='javascript prototypes website'></img>
+        </a>
+      </div>
     </div>
   )
 }
