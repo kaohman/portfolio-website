ad341bb26f324da0a470f9370b6a836726776fe1
Karin Ohman <kaohma@gmail.com>
2019-01-14 18:15:47 -0700

    Updates

---
+++ precache-manifest.4b3c15cc87a088629769368a92dc18ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b256896c0c48cf1d86c",
+    "url": "/portfolio-website/static/js/runtime~main.2b256896.js"
+  },
+  {
+    "revision": "a9e8e558fd1d8972f83c",
+    "url": "/portfolio-website/static/js/main.a9e8e558.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/portfolio-website/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "a9e8e558fd1d8972f83c",
+    "url": "/portfolio-website/static/css/main.b190acc7.chunk.css"
+  },
+  {
+    "revision": "6835e49b2fc03327e0680a21452e6567",
+    "url": "/portfolio-website/index.html"
+  }
+];
