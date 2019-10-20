b852d4619f52c5ede93203846cdf36de44770cc4
Karin Ohman <kaohma@gmail.com>
2019-10-20 11:23:30 -0600

    Update projects

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ function About() {
           <p>I am a Denver based web developer passionate about front-end development as a means to convey complicated data and information to a broader audience. I'm excited by innovation and creating products and solutions from scratch with code. My background working with scientific data and data analysis have taught me that how you display and communicate information greatly impacts how powerful an impact it will have on the audience.</p>
           <p>As a front-end engineer, I am proficient in HTML, CSS (including SASS), and Javascript. I have a strong foundation in Reactjs, DOM manipulation, and Object-Oriented Programming. I also have knowledge of UX/UI principles to build desktop, mobile and web apps. Additionally, I am proficient with team collaboration using git, GitHub, agile workflow, and Atlassian software.</p>
           <p>My educational background and experience are in coastal and climate change resiliency so I am particularly passionate about those topics. I have worked extensively with Geographic Information Systems (GIS) software as a useful tool to analyze and visualize data.</p>
-          <p>Outside of coding, I enjoy many outdoor activities including mountain biking, hiking, camping. I also play soccer, am a casual CrossFitter, and love knitting.</p>
+          <p>Outside of coding, I enjoy many outdoor activities including mountain biking, hiking, camping. I also play soccer, run, and love knitting.</p>
         </div>
       </div>
     </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,27 +5,31 @@ function Portfolio() {
     <div className='page-padding-div'>
       <h1>portfolio</h1>
       <div id='portfolio-content'>
-        <a href='https://github.com/kaohman/bike-see'>
+        <a href='https://github.com/kaohman/bike-see' target='_blank'>
           <h3>bikeSee</h3>
           <img className='screenshots' src='./assets/bikesee-screenshot.png' alt='bike see website'></img>
         </a>
-        <a href='https://github.com/kaohman/national-parks'>
+        <a href='https://github.com/kaohman/sea-level-rise-data' target='_blank'>
+          <h3>sea level rise api</h3>
+          <img className='screenshots' src='./assets/sea-level-rise-screenshot.png' alt='sea level rise website'></img>
+        </a>
+        <a href='https://github.com/kaohman/nasa-apod' target='_blank'>
+          <h3>nasa apod</h3>
+          <img className='screenshots' src='./assets/nasa-apod-screenshot.png' alt='nasa astronomy photo of the days website'></img>
+        </a>
+        <a href='https://github.com/kaohman/national-parks' target='_blank'>
           <h3>mark my parks</h3>
           <img className='screenshots' src='./assets/parks-screenshot.png' alt='mark my parks website'></img>
         </a>
-        <a href='https://github.com/kaohman/listy'>
+        <a href='https://github.com/kaohman/listy' target='_blank'>
           <h3>listy</h3>
           <img className='screenshots' src='./assets/listy-screenshot.png' alt='listy website'></img>
         </a>
-        <a href='https://github.com/kaohman/jeopardy'>
+        <a href='https://github.com/kaohman/jeopardy' target='_blank'>
           <h3>space jeopardy</h3>
           <img className='screenshots' src='./assets/jeopardy-screenshot.png' alt='sapce jeopardy website'></img>
         </a>
-        <a href='https://github.com/kaohman/movie-tracker'>
-          <h3>movie tracker</h3>
-          <img className='screenshots' src='./assets/movie-tracker-screenshot.png' alt='movie tracker website'></img>
-        </a>
-        <a href='https://github.com/kaohman/js-proto-fun-time'>
+        <a href='https://github.com/kaohman/js-proto-fun-time' target='_blank'>
           <h3>jsProtoFunTime</h3>
           <img className='screenshots' src='./assets/prototypes-screenshot.png' alt='javascript prototypes website'></img>
         </a>
